6a6d159bd9ad88010c145397cff74e5b5c0044cd
evanalifian <evanalifian@gmal.com>
2025-01-18 15:17:01 +0700

    update style on wrapper and update category

--- src/components/molecules/BlogItemCard.jsx
+++ src/components/molecules/BlogItemCard.jsx
@@ -1,15 +1,15 @@
-import LabelName from "../atoms/LabelName";
+import LabelName from "../atoms/LabelText";
 import { getSlug } from "../../assets/functions";
 import { Link } from "react-router";
 
 export default function BlogItemCard({ category, title }) {
   return (
     <div
-      className="border border-zinc-300 rounded-lg bg-white p-4 transition-colors hover:border-zinc-400/70"
+      className="flex flex-col  gap-y-1 border border-zinc-300 rounded-lg bg-white p-4 transition-colors hover:border-zinc-400/70"
       title={title}
     >
-      <LabelName>{category}</LabelName>
-      <Link to={`/blog/${category.toLowerCase()}/${getSlug(title)}`}>
+      <LabelName>Category: {category}</LabelName>
+      <Link to={`/blog/${category.toLowerCase()}/${getSlug(title)}`} className="whitespace-pre text-ellipsis overflow-hidden font-medium">
         {title}
       </Link>
     </div>
