87ba168d1c8dc87c4df4da446caf3d15d6e8ab73
evanalifian <evanalifian@gmal.com>
2025-01-28 21:17:29 +0700

    delete unused react components

--- src/components/atoms/LabelText.jsx
+++
@@ -1,5 +0,0 @@
-export default function LabelText({ children }) {
-  return (
-    <span className="label_text">{children}</span>
-  )
-}
--- src/components/atoms/WelcomingText.jsx
+++
@@ -1,7 +0,0 @@
-export default function WelcomingText({ children }) {
-  return (
-    <h1 className="text-4xl leading-tight font-bold bg-clip-text text-transparent bg-gradient-to-t from-zinc-700 to-zinc-500 my-0">
-      {children}
-    </h1>
-  );
-}
