a868ad2558fd91ce888d0b4d015de989503178c6
evanalifian <evanalifian@gmal.com>
2025-01-28 21:18:34 +0700

    adding description key

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -21,9 +21,11 @@ export const blogs = [
   {
     title: "Techs That I Use To Make Website",
     category: "Techs",
+    desc: "I share my tech stacks for web development. I still learn how to make website properly."
   },
   {
     title: "Hello, World!",
     category: "Talks",
+    desc: "Just want to say \"HI\" to you."
   },
 ];
