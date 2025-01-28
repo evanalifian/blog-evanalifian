6a2b59a335c069a7b854e435d9e6e3c355eb2189
evanalifian <evanalifian@gmal.com>
2025-01-28 21:20:41 +0700

    delete Home and BlogView element and delete /blog route

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import "./blog.css";
-import MainTemplateLayout from "./components/templates/MainTemplateWrap";
-import Home from "./pages/home/Home";
-import Blog from "./pages/blogs/Blog";
-import BlogView from "./pages/blogs/BlogView";
+import Home from "./pages/home/Page";
+import BlogView from "./pages/blogs/Page";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:category/:title" element={<BlogView />} />
       </Routes>
     </BrowserRouter>
